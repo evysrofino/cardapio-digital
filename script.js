d86b0b0e97ac624afd8e4ccfcf94bb2e4206c170
Evelyn Rofino <evelyn_rofino@hotmail.com>
2026-08-27 00:18:35 -0300

    feat: exibe itens e total do carrinho

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let carrinho = [];
 
 const botaoXSalada = document.getElementById("btn-xsalada");
+const itensCarrinho = document.getElementById("itens-carrinho");
+const totalCarrinho = document.getElementById("total-carrinho");
 
 botaoXSalada.addEventListener("click", function () {
 
@@ -9,7 +11,27 @@ botaoXSalada.addEventListener("click", function () {
         preco: 25.00
     });
 
-    console.log(carrinho);
-
-    alert("X-Salada adicionado ao pedido!");
+    atualizarCarrinho();
 });
+
+function atualizarCarrinho() {
+
+    itensCarrinho.innerHTML = "";
+
+    let total = 0;
+
+    carrinho.forEach(function (produto) {
+
+        const item = document.createElement("p");
+
+        item.textContent =
+            produto.nome + " - R$ " + produto.preco.toFixed(2);
+
+        itensCarrinho.appendChild(item);
+
+        total = total + produto.preco;
+    });
+
+    totalCarrinho.textContent =
+        "Total: R$ " + total.toFixed(2);
+}
