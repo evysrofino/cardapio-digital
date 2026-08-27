017a689cf62fcd082c4389e8d520d2159fe694e8
Evelyn Rofino <evelyn_rofino@hotmail.com>
2026-08-27 00:11:15 -0300

    feat: inicia estrutura do carrinho

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+let carrinho = [];
+
 const botaoXSalada = document.getElementById("btn-xsalada");
 
 botaoXSalada.addEventListener("click", function () {
+
+    carrinho.push({
+        nome: "X-Salada",
+        preco: 25.00
+    });
+
+    console.log(carrinho);
+
     alert("X-Salada adicionado ao pedido!");
 });
